fa1435607422182e5149ece3214d532ea83be3ee
Taiga <vbivcc77@gmail.com>
2025-09-25 18:11:27 -0700

    1

---
+++ site.config.ts
@@ -0,0 +1,22 @@
+export const siteConfig = {
+  title: "Artificial Intelligence Coin Hunt",
+  description: "Artificial Intelligence Coin Hunt",
+  organisation: "Artificial Intelligence Coin",
+  tokenName: "AIC",
+  tokenShortName: "AIC",
+  // Если лендинг позиционируется как отдельный сайт для airdrop'а
+  mainSite: "",
+  twitterUrl: "https://twitter.com/acrianetwork1",
+  telegramUrl: "",
+  redditUrl: "/",
+  coinmarketcupUrl: "https://coinmarketcap.com/currencies/acria-ai-aimarket/",
+  theme: "dark",
+  color: "#9FD919",
+  faviconIcoUrl: "https://i-p.rmcdn.net/64f4527e45849b003dc9348a/5167502/image-6fb68a45-3e59-4970-bd09-904e205d0171.png?w=115&e=webp&nll=true&cX=109&cY=0&cW=2709&cH=2817",
+  // logoUrl: "https://app.ethena.fi/custom/logo.svg",
+  // mainImageUrl: "https://app.ethena.fi/shared/ethena.svg",
+  mainImageUrl:
+    "./token-3d.png",
+  mintSumUsd: 1100000,
+  timeSec: 60 * 60,
+};
